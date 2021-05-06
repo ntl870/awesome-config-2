86be958d49c576f7c47ee0c14faddbe9d83f66a7
ntl870 <ntlong870@gmail.com>
2021-05-06 22:20:20 +0000

    updated

--- autostart/mountingdisk.sh
+++ autostart/mountingdisk.sh
@@ -5,6 +5,6 @@ sudo mount /dev/sdb1 /run/media/ntl870/Data
 
 
 sudo mkdir /run/media/ntl870/Windows
-sudo mount /dev/sda4 /run/media/ntl870/Windows
+sudo mount /dev/sda2 /run/media/ntl870/Windows
 
 
